fefc6f2475f499ffc86147cc25e847c1434600da
Tam Vo <vo.mita.ov@gmail.com>
2013-01-29 07:36:07 +0700

    Retry connect to prevent Connection Timeout

--- ftp_upload.rb
+++ ftp_upload.rb
@@ -18,6 +18,19 @@ module FTPExt
   end
 end
 
+def connect_ftp(ftp_host, ftp_username, ftp_password)
+  begin
+    ftp = BetterFTP.new(ftp_host, ftp_username, ftp_password)
+  rescue Net::FTPPermError => e
+    puts e.message
+    puts e.backtrace.join("\n")
+    abort
+  end
+  ftp.extend(FTPExt)
+  ftp.passive = true
+  ftp
+end
+
 config = YAML.load_file "#{File.expand_path(Dir.pwd)}/ftp_config.yml"
 debug = config["debug"]
 ftp_host = config["ftp_host"]
@@ -25,17 +38,7 @@ ftp_username = config["ftp_username"]
 ftp_password = config["ftp_password"]
 ftp_public_html = config["ftp_public_html"]
 
-begin
-  ftp = BetterFTP.new(ftp_host, ftp_username, ftp_password)
-rescue Net::FTPPermError => e
-  puts e.message
-  puts e.backtrace.join("\n")
-  abort
-end
-ftp.extend(FTPExt)
-
-ftp.passive = true
-
+ftp = connect_ftp(ftp_host, ftp_username, ftp_password)
 sha_from = ARGV[0]
 sha_to = ARGV[1]
 
@@ -46,19 +49,33 @@ end
 result = `git show --pretty='format:' --name-only #{sha_from}..#{sha_to}`
 
 files = result.strip.split("\n").reject { |file| file.empty? }
+num_try = 0
 files.each do |file|
-  remote_path = ftp_public_html + File.dirname(file)
-  if File.exists? file
-    puts "- Uploading #{file}" if debug
-    ftp.chdirc(remote_path)
-    ftp.putbinaryfile(file, File.basename(File.basename(file)))
-  else
-    puts "- Deleting #{file}" if debug
-    ftp.try_rm remote_path
+  begin
+    remote_path = ftp_public_html + File.dirname(file)
+    if File.exists? file
+      puts "- Uploading #{file}" if debug
+      ftp.chdirc(remote_path)
+      ftp.putbinaryfile(file, File.basename(File.basename(file)))
+    else
+      puts "- Deleting #{file}" if debug
+      ftp.try_rm remote_path
+    end
+    puts ftp.last_response if debug
+  rescue Errno::ETIMEDOUT => e
+    num_try += 1
+    puts e.message
+    puts e.backtrace.join("\n")
+    puts "TRY AGAIN: #{num_try}"
+
+    if num_try > 2
+      `say "Fail Fail Fail"`
+    else
+      ftp.try(:quit)
+      ftp = connect_ftp(ftp_host, ftp_username, ftp_password)
+    end
   end
-  puts ftp.last_response if debug
 end
-ftp.quit
 
 `say "Upload FTP Done"`
 
